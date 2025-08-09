23374931bafda6c29ece03eaf485cd1c43a123c5
haneef <haneefsyed0000@gmail.com>
2025-08-09 15:55:34 +0500

    resume

--- resume/settings.py
+++ resume/settings.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "haneefsyed0000@gmail.com"
+password = "admin123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
